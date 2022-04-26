74301180541b1aa07142cc9867c3f4cb35731a7c
“kyawt173” <kyawt.hlaing@atkhayar.com>
2022-04-26 21:53:48 +0630

    Initial Commit

---
+++ e-market/e_marketApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_marketApp.swift
+//  e-market
+//
+//  Created by Kyawt May Hlaing on 26/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct e_marketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
