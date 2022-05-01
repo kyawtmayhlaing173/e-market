4c84ca176e5c50cdee0292476d5a3f9a53bf8304
“kyawt173” <kyawt.hlaing@atkhayar.com>
2022-05-01 20:01:42 +0630

    Add unit test for cart controller

--- e-marketTests/e_marketTests.swift
+++ e-marketTests/e_marketTests.swift
@@ -9,28 +9,39 @@ import XCTest
 @testable import e_market
 
 class e_marketTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    private var cart: CartController!
+    
+    override func setUp() {
+        cart = CartController()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testAddProductCountToCartCalculatingTotalPrice() {
+        cart.addToCart(product: Product(name: "Latte", price: 50, imageUrl: "https://www.nespresso.com/ncp/res/uploads/recipes/nespresso-recipes-Latte-Art-Tulip.jpg"))
+        cart.addToCart(product: Product(name: "Dark Tiramisu Mocha", price: 75, imageUrl: "https://www.nespresso.com/shared_res/mos/free_html/sg/b2b/b2ccoffeerecipes/listing-image/image/dark-tiramisu-mocha.jpg"))
+        XCTAssertEqual(cart.total, 125)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testDecreaseProductCountFromCartCalculatingTotalPrice() {
+        cart.addToCart(product: Product(name: "Latte", price: 50, imageUrl: "https://www.nespresso.com/ncp/res/uploads/recipes/nespresso-recipes-Latte-Art-Tulip.jpg"))
+        cart.addToCart(product: Product(name: "Latte", price: 50, imageUrl: "https://www.nespresso.com/ncp/res/uploads/recipes/nespresso-recipes-Latte-Art-Tulip.jpg"))
+        cart.addToCart(product: Product(name: "Dark Tiramisu Mocha", price: 75, imageUrl: "https://www.nespresso.com/shared_res/mos/free_html/sg/b2b/b2ccoffeerecipes/listing-image/image/dark-tiramisu-mocha.jpg"))
+        cart.decreaseProductCount(product: Product(name: "Latte", price: 50, imageUrl: "https://www.nespresso.com/ncp/res/uploads/recipes/nespresso-recipes-Latte-Art-Tulip.jpg"))
+        XCTAssertEqual(cart.total, 125)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testRemoveProductFromCart() {
+        cart.addToCart(product: Product(name: "Latte", price: 50, imageUrl: "https://www.nespresso.com/ncp/res/uploads/recipes/nespresso-recipes-Latte-Art-Tulip.jpg"))
+        cart.addToCart(product: Product(name: "Dark Tiramisu Mocha", price: 75, imageUrl: "https://www.nespresso.com/shared_res/mos/free_html/sg/b2b/b2ccoffeerecipes/listing-image/image/dark-tiramisu-mocha.jpg"))
+        cart.addToCart(product: Product(name: "Latte", price: 50, imageUrl: "https://www.nespresso.com/ncp/res/uploads/recipes/nespresso-recipes-Latte-Art-Tulip.jpg"))
+        cart.removeFromCart(product: Product(name: "Latte", price: 50, imageUrl: "https://www.nespresso.com/ncp/res/uploads/recipes/nespresso-recipes-Latte-Art-Tulip.jpg"))
+        XCTAssertEqual(cart.total, 75)
     }
-
+    
+    func testClearCart() {
+        cart.addToCart(product: Product(name: "Latte", price: 50, imageUrl: "https://www.nespresso.com/ncp/res/uploads/recipes/nespresso-recipes-Latte-Art-Tulip.jpg"))
+        cart.addToCart(product: Product(name: "Dark Tiramisu Mocha", price: 75, imageUrl: "https://www.nespresso.com/shared_res/mos/free_html/sg/b2b/b2ccoffeerecipes/listing-image/image/dark-tiramisu-mocha.jpg"))
+        XCTAssertEqual(cart.total, 0)
+    }
+    
 }
